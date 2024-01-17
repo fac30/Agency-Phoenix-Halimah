efaacd61017c05d4e9500271d1b716f05835f9bd
RayFFH <57190209+RayFFH@users.noreply.github.com>
2024-01-17 15:04:13 +0000

    New Carousel and pop overs have been fixed

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var testimonialCarousel = new bootstrap.Carousel(document.getElementById('testimonialCarousel'), {
+      interval: false // Disable automatic sliding for manual control
+    });
+  });
